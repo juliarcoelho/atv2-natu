e29513e8e5836eab5515c3df026283e473da1761
juliarcoelho <juliarochx@gmail.com>
2022-05-21 22:05:56 -0300

    correção na lógica do js e incremento de carrossel no index html

--- script.js
+++ script.js
@@ -1,64 +1,97 @@
-//validar o email 
-//validar campos de preenchimento obrigatório
-//mensagem de erro acima dos campos do formulário
+//carrossel de slides do topo da página
 //carrossel com botões na direita e na esquerda
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//validar o email e telefone
+//validar campos de preenchimento obrigatório
+//mensagem de erro acima ou abaixo dos campos do formulário
 //precisa de js pra segunda pág? não foi informado na atividade.
+function validaNome(){
+  var nome = document.getElementById('nome_formulario');
 
-function validar_formulario(form){
-
-  var invalid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-  
-  var nome = document.getElementById('nome');
-  var email = document.getElementById('email');
-  var telefone = document.getElementById('telefone');
-
-  var erro_nome = document.querySelector('.msg-nome');
-  erro_nome.style.display = 'none';
-
-  var erro_email = document.querySelector('.msg-email');
-  erro_email.style.display = 'none';
-
-  var erro_telefone = document.querySelector('.msg-telefone');
-  erro_telefone.style.display = 'none';
+  var erro_nome_formulario = document.querySelector('.msg-nome');
+  erro_nome_formulario.style.display = 'none';
 
   if (nome_formulario.value==""){
     erro_nome_formulario.innerHTML = "Nome é obrigatório";
     erro_nome_formulario.style.display = 'block';
     nome_formulario.focus();
     return false;
+  } else {
+    return true;
   }
+}
 
-  if (email.value==""){
-    erro_email.innerHTML = "Email é obrigatório";
+function validaEmail(){
+
+  var email = document.getElementById('email');
+
+  var erro_email = document.querySelector('.msg-email');
+  erro_email.style.display = 'none';
+
+  if (email.value=="" || email.value.indexOf('@')== -1 || email.value.indexOf('.')== -1){
+    erro_email.innerHTML = "Preencha corretamente seu email";
     erro_email.style.display = 'block';
     email.focus();
     return false;
+  } else {
+    return true;
   }
+}
 
-  if (invalid.test(email.value==false)){
-    erro_email.innerHTML = "Email informado incorreto";
-    erro_email.style.display = 'block';
-    email.focus();
-    return false;
-  }
 
-  if (telefone.value==""){
+function validaTelefone(){
+  var telefone = document.getElementById('telefone');
+
+  var erro_telefone = document.querySelector('.msg-telefone');
+  erro_telefone.style.display = 'none';
+
+  if (telefone.value=="" || telefone.value.indexOf('9')== -1 || telefone.length<11){
     erro_telefone.innerHTML = "Telefone é obrigatório";
     erro_telefone.style.display = 'block';
     telefone.focus();
     return false;
+  } else {
+    return true;
   }
-
-  //if (invalidTelefone.test(telefone.value==false)){
-   // erro_telefone.innerHTML = "Telefone informado incorreto";
-   // erro_telefone.style.display = 'block';
-   // telefone.focus();
-   // return false;
- // }
-
-  return false;
 }
 
+function validar_formulario(form){
 
+ if (validaNome() == false){
+   return false;
+ } else if (validaEmail() == false) {
+   return false;
+ } else if (validaTelefone() == false){
+    return false;
+ } else {
+   return true;
+ }
 
+}
 
